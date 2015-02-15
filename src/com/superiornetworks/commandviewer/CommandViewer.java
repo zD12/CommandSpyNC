908689f06634db65fc698bbe41b4e66108b3bf38
Ryan <Admin@superior-networks.com>
2015-02-15 13:17:26 +0000

    Complete re-write of the plugin. Commands are no longer required as it is all fully automated based on permissions. Resolves #2

---
+++ src/com/superiornetworks/commandviewer/CommandViewer.java
@@ -0,0 +1,60 @@
+package com.superiornetworks.commandviewer;
+
+import net.pravian.bukkitlib.BukkitLib;
+import net.pravian.bukkitlib.command.BukkitCommandHandler;
+import net.pravian.bukkitlib.config.YamlConfig;
+import net.pravian.bukkitlib.implementation.BukkitPlugin;
+import net.pravian.bukkitlib.util.LoggerUtils;
+import org.bukkit.plugin.PluginManager;
+import com.superiornetworks.commandviewer.listeners.PlayerListener;
+import java.util.List;
+
+public class CommandViewer extends BukkitPlugin
+{
+
+    private CommandViewer plugin;
+    public BukkitCommandHandler handler;
+
+    // YAML Files
+    public YamlConfig config;
+    public YamlConfig players;
+    
+    public List<String> allowedplayers;
+
+    @Override
+    public void onLoad()
+    {
+        this.plugin = this;
+        this.handler = new BukkitCommandHandler(plugin);
+    }
+
+    @Override
+    public void onEnable()
+    {
+        BukkitLib.init(plugin);
+      //  handler.setCommandLocation(Command_cmdviewer.class.getPackage());
+        this.config = new YamlConfig(plugin, "config.yml");
+        this.players = new YamlConfig(plugin, "players.yml");
+        config.load();
+        players.load();
+        final PluginManager pm = plugin.getServer().getPluginManager();
+        pm.registerEvents(new PlayerListener(plugin), plugin);
+        allowedplayers = (List<String>) plugin.players.getList("playerlist");
+
+        LoggerUtils.info(plugin, "Has been created by Wild1145 - Check out www.superior-networks.com for great value servers!");
+    }
+
+    @Override
+    public void onDisable()
+    {
+        config.save();
+        LoggerUtils.info(plugin, "Has been created by Wild1145 - Check out www.superior-networks.com for great value servers!");
+    }
+
+  /*  @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args)
+    {
+        return handler.handleCommand(sender, cmd, commandLabel, args);
+    }
+*/
+}
---
+++ src/com/superiornetworks/commandviewer/listeners/PlayerListener.java
@@ -0,0 +1,72 @@
+package com.superiornetworks.commandviewer.listeners;
+
+import com.superiornetworks.commandviewer.CommandViewer;
+import java.util.List;
+import net.pravian.bukkitlib.util.LoggerUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class PlayerListener implements Listener
+{
+
+    private final CommandViewer plugin;
+
+    public PlayerListener(CommandViewer plugin)
+    {
+        this.plugin = plugin;
+    }
+
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event)
+    {
+        String command = event.getMessage();
+        final Player player = event.getPlayer();
+
+        Bukkit.getOnlinePlayers().stream().filter((pl) -> (plugin.allowedplayers.contains(pl.getName()))).forEach((pl) ->
+        {
+            pl.sendMessage(ChatColor.GRAY + player.getName() + " : " + command);
+        });
+
+        // This is for Debugging only now
+        // LoggerUtils.info(plugin, command);
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onUncancelledPlayerJoin(PlayerJoinEvent event)
+    {
+        final Player player = event.getPlayer();
+
+        if (player.hasPermission("commandviewer.viewcommands"))
+        {
+            if (plugin.allowedplayers.contains(player.getName()))
+            {
+                LoggerUtils.info(plugin, player.getName() + "is already authrised");
+            }
+            else
+            {
+                plugin.allowedplayers.add(player.getName());
+                LoggerUtils.info(plugin, player.getName() + "has been added to the authrised players list.");
+            }
+        }
+        else
+        {
+            if (plugin.allowedplayers.contains(player.getName()))
+            {
+                plugin.allowedplayers.remove(player.getName());
+                LoggerUtils.info(plugin, player.getName() + "Has been removed from the authrised players list");
+            }
+            else
+            {
+                LoggerUtils.info(plugin, player.getName() + "does not have the required permissions and was not already aurhtised. ");
+            }
+        }
+        plugin.players.save();
+    }
+
+}
